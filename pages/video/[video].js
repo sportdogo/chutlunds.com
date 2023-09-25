5afe711e254a8fbb8e711fc7df7ea1a06bd4bbe4
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2023-09-25 16:44:36 +0530

    commit

--- pages/video/[video].js
+++ pages/video/[video].js
@@ -9,13 +9,24 @@ import { getCookie, setCookie } from "cookies-next";
 import VideoPlayer from '../../components/VideoPlayer';
 import MultiformatAds from '../../components/Ads/MultiFormatAds';
 import Outstreams from '../../components/Ads/Outstream';
-import { getVideoPageData } from '../../config/videoPlayer';
+// import { getVideoPageData } from '../../config/videoPlayer';
+import { BeatLoader } from 'react-spinners'
 
 
-function Videoplayer({ serverError, videolink_qualities_screenshots, preloaded_video_quality, relatedVideos, pornstar, video_details, videoTitleBackUp, noVideo }) {
+function Videoplayer() {
 
     const router = useRouter()
+    const { video } = router.query;
 
+    const [spinnerLoading, setspinnerLoading] = useState(true)
+    const [serverError, setServerError] = useState(false);
+    const [videolink_qualities_screenshots, setVideolinkQualitiesScreenshots] = useState({});
+    const [preloaded_video_quality, setPreloadedVideoQuality] = useState('');
+    const [relatedVideos, setRelatedVideos] = useState([]);
+    const [pornstar, setPornstar] = useState([]);
+    const [video_details, setVideoDetails] = useState({});
+    const [videoTitleBackUp, setVideoTitleBackUp] = useState("");
+    const [noVideo, setNoVideo] = useState(false);
 
 
     const [Quality, setQuality] = useState(preloaded_video_quality)
@@ -31,32 +42,34 @@ function Videoplayer({ serverError, videolink_qualities_screenshots, preloaded_v
     const [showNotAvailableMessage, setshowNotAvailableMessage] = useState(false);
 
     useEffect(() => {
+        if (!router.isReady) return;
 
-        let uniqarray = [...new Set(videolink_qualities_screenshots.tagsArray)];
-        settags(uniqarray)
-
-        // Create single string of all tags using comma
-        let tagsString = ''
-        uniqarray.map((tag, index) => {
-            if (index === 0) {
-                tagsString = tag
-            } else {
-                tagsString = tagsString + ", " + tag
-            }
-        })
-        settagString(tagsString);
+        const keyy = video.substring(video.indexOf("video/"), video.indexOf("*"))
+        const title = video.substring(video.indexOf("*") + 1, video.length).trim();
+        setVideoTitleBackUp(title)
+        const fetchVideoDetails = async () => {
 
 
-        setTimeout(() => {
-            setshowNotAvailableMessage(true)
-        }, 3000);
+            const rawResponse = await fetch(`https://clownfish-app-jn7w9.ondigitalocean.app/getVideoPageDetails`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ href: `https://spankbang.party/${keyy}/video/${title}` })
 
-
-        const emailExists = getCookie("email");
-        if (typeof emailExists !== 'undefined' && emailExists.length > 4) {
-            setloggedIn(true)
+            });
+            const resData = await rawResponse.json();
+            const data = resData.data;
+            // { serverError, videolink_qualities_screenshots, preloaded_video_quality, relatedVideos, pornstar, video_details, videoTitleBackUp, noVideo }
+            setVideolinkQualitiesScreenshots(data.videolink_qualities_screenshots)
+            setPreloadedVideoQuality(data.preloaded_video_quality)
+            setRelatedVideos(data.relatedVideos)
+            setPornstar(data.pornstar)
+            setVideoDetails(data.video_details)
+            setNoVideo(data.noVideo)
+            setspinnerLoading(false)
         }
-
         async function fetchCountryVideos() {
 
             const url2 = "https://spankbang.party/s/spangbang/?o=new"
@@ -85,11 +98,55 @@ function Videoplayer({ serverError, videolink_qualities_screenshots, preloaded_v
             setcountryVideo(content.data.finalDataArray)
 
         }
+        try {
+            fetchVideoDetails()
+        } catch (error) {
+            setServerError(true)
+        }
 
-        // fetchCountryVideos()
 
-    }, []);
+        let uniqarray = [...new Set(videolink_qualities_screenshots.tagsArray)];
+        settags(uniqarray)
 
+        // Create single string of all tags using comma
+        let tagsString = ''
+        uniqarray.map((tag, index) => {
+            if (index === 0) {
+                tagsString = tag
+            } else {
+                tagsString = tagsString + ", " + tag
+            }
+        })
+        settagString(tagsString);
+
+
+        setTimeout(() => {
+            setshowNotAvailableMessage(true)
+        }, 3000);
+
+
+        const emailExists = getCookie("email");
+        if (typeof emailExists !== 'undefined' && emailExists.length > 4) {
+            setloggedIn(true)
+        }
+
+
+
+
+    }, [router.isReady]);
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+
+    if (spinnerLoading) {
+        return (
+            <div className="flex justify-center mx-auto mt-10 h-screen mt-[100px]">
+                <BeatLoader loading size={25} color={'#13274F'} />
+            </div>
+        )
+    }
 
     if (serverError) {
         return (
@@ -99,83 +156,81 @@ function Videoplayer({ serverError, videolink_qualities_screenshots, preloaded_v
             </div>
         )
     }
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
 
 
-    return (
-        <div className="">
+    if (preloaded_video_quality.length > 0) {
+        return (
+            <div className="">
 
 
-            {noVideo &&
-                <Head>
-                    <title>{`${videoTitleBackUp}-720p`} | Chutlunds</title>
-                    <meta name="description" content={`${capitalizeFirstLetter(videoTitleBackUp)} sex video.`} />
-                    <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-                </Head>
-            }
+                {noVideo &&
+                    <Head>
+                        <title>{`${videoTitleBackUp}-720p`} | Chutlunds</title>
+                        <meta name="description" content={`${capitalizeFirstLetter(videoTitleBackUp)} sex video.`} />
+                        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+                    </Head>
+                }
 
-            {!noVideo &&
+                {!noVideo &&
 
-                <Head>
-                    <title>{`${video_details.Title}- ${videolink_qualities_screenshots.video_qualities_available[videolink_qualities_screenshots.video_qualities_available.length - 1].toUpperCase()}`} | Chutlunds</title>
-                    <meta name="description" content={`${capitalizeFirstLetter(video_details.Title)} , ${tagString} sex videos.`} />
-                    <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-                </Head>
-            }
+                    <Head>
+                        <title>{`${video_details.Title}- ${videolink_qualities_screenshots.video_qualities_available[videolink_qualities_screenshots.video_qualities_available.length - 1].toUpperCase()}`} | Chutlunds</title>
+                        <meta name="description" content={`${capitalizeFirstLetter(video_details.Title)} , ${tagString} sex videos.`} />
+                        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+                    </Head>
+                }
 
-            {!noVideo &&
-                <div>
+                {!noVideo &&
+                    <div>
 
-                    <div className='flex text-sm md:text-lg '>
+                        <div className='flex text-sm md:text-lg '>
 
-                        <div className='flex items-center mt-2 space-x-1 lg:space-x-2'>
-                            <FilmIcon className='h-[20px] md:h-9 hover:scale-100 text-red-600' />
-                            {videolink_qualities_screenshots.video_qualities_available.map(quality => {
-                                return (
-                                    <p key={quality} className='font-poppins pr-1'>{quality.toUpperCase()}</p>
+                            <div className='flex items-center mt-2 space-x-1 lg:space-x-2'>
+                                <FilmIcon className='h-[20px] md:h-9 hover:scale-100 text-red-600' />
+                                {videolink_qualities_screenshots.video_qualities_available.map(quality => {
+                                    return (
+                                        <p key={quality} className='font-poppins pr-1'>{quality.toUpperCase()}</p>
 
-                                )
-                            })}
+                                    )
+                                })}
+                            </div>
+
                         </div>
 
-                    </div>
 
 
 
-
-                    <h1 className='text-md sm:text-lg font-semibold my-1 text-wrap text-gray-700 md:text-2xl font-inter'>{video_details.Title}</h1>
-
-
-                    <div className='py-1  rounded overflow-hidden sm:cursor-pointer md:w-4/5'>
+                        <h1 className='text-md sm:text-lg font-semibold my-1 text-wrap text-gray-700 md:text-2xl font-inter'>{video_details.Title}</h1>
 
 
-                        <VideoPlayer video_details={video_details} VideoSrc={VideoSrc} Qualitys={Quality} videolink_qualities_screenshots={videolink_qualities_screenshots} preloaded_video_quality={preloaded_video_quality} pornstar={pornstar} loggedIn={loggedIn} />
+                        <div className='py-1  rounded overflow-hidden sm:cursor-pointer md:w-4/5'>
 
 
-                    </div>
+                            <VideoPlayer video_details={video_details} VideoSrc={VideoSrc} Qualitys={Quality} videolink_qualities_screenshots={videolink_qualities_screenshots} preloaded_video_quality={preloaded_video_quality} pornstar={pornstar} loggedIn={loggedIn} />
+
+
+                        </div>
 
 
 
 
 
-                    <MultiformatAds />
+                        <MultiformatAds />
 
-                    <div className='flex flex-col p-1 px-3 space-x-2  items-center md:flex-row sm:justify-items-start'>
-                        <p className='font-semibold text-button text-[18px] lg:text-[24px] font-manrope'>Videos related to</p>
-                        <p className='font-semibold text-[15px] lg:text-[20px] pl-1 font-inter'>{video_details.Title}</p>
-                    </div>
-                    <Videos data={relatedVideos} />
+                        <div className='flex flex-col p-1 px-3 space-x-2  items-center md:flex-row sm:justify-items-start'>
+                            <p className='font-semibold text-button text-[18px] lg:text-[24px] font-manrope'>Videos related to</p>
+                            <p className='font-semibold text-[15px] lg:text-[20px] pl-1 font-inter'>{video_details.Title}</p>
+                        </div>
+                        <Videos data={relatedVideos} />
 
 
-                    <MultiformatAds />
-                    <Outstreams />
-                    {latestVideo.length !== 0 &&
-                        <Videos data={latestVideo} />
-                    }
-                    <MultiformatAds />
-                    {/* {countryVideo.length !== 0 &&
+                        <MultiformatAds />
+                        <Outstreams />
+                        {latestVideo.length !== 0 &&
+                            <Videos data={latestVideo} />
+                        }
+                        <MultiformatAds />
+                        {/* {countryVideo.length !== 0 &&
                         <Videos data={countryVideo} />
                     }
 
@@ -183,109 +238,50 @@ function Videoplayer({ serverError, videolink_qualities_screenshots, preloaded_v
                     <MultiformatAds />
                */}
 
-                </div>
-            }
+                    </div>
+                }
 
-            {noVideo &&
+                {noVideo &&
 
-                < div >
-                    {relatedVideos.length !== 0 &&
+                    < div >
+                        {relatedVideos.length !== 0 &&
 
-                        <div className='relative'>
+                            <div className='relative'>
 
-                            <h1 className='my-10 mb-16 font-semibold font-inter lg:text-xl text-center'> {videoTitleBackUp.replaceAll("+", " ")}</h1>
+                                <h1 className='my-10 mb-16 font-semibold font-inter lg:text-xl text-center'> {videoTitleBackUp.replaceAll("+", " ")}</h1>
 
 
 
-                            <h2 className=' font-poppins md:text-lg'>Related videos to {videoTitleBackUp.replaceAll("+", " ")}</h2>
+                                <h2 className=' font-poppins md:text-lg'>Related videos to {videoTitleBackUp.replaceAll("+", " ")}</h2>
 
-                            <Videos data={relatedVideos} />
+                                <Videos data={relatedVideos} />
 
-                            {showNotAvailableMessage &&
+                                {showNotAvailableMessage &&
 
-                                <div className='flex space-x-4 items-center justify-center absolute top-[35px] mx-auto left-0 right-0'>
-                                    <InformationCircleIcon className='h-8 text-black' />
-                                    <span className=' text-sm font-semibold font-inter lg:text-lg  text-theme w-fit'> This Video Is No Longer Available.</span>
-                                </div>
-                            }
-                        </div>
+                                    <div className='flex space-x-4 items-center justify-center absolute top-[35px] mx-auto left-0 right-0'>
+                                        <InformationCircleIcon className='h-8 text-black' />
+                                        <span className=' text-sm font-semibold font-inter lg:text-lg  text-theme w-fit'> This Video Is No Longer Available.</span>
+                                    </div>
+                                }
+                            </div>
 
-                    }
-                </div>
+                        }
+                    </div>
 
-            }
-        </div >
+                }
+            </div >
 
-    )
+
+        )
+    }
+
 }
-
-
-
 
 
 export default Videoplayer
 
 
-export async function getServerSideProps(context) {
 
-    const { video } = context.query;
-    const keyy = video.substring(video.indexOf("video/"), video.indexOf("*"))
-    const title = video.substring(video.indexOf("*") + 1, video.length).trim();
-
-
-    var videolink_qualities_screenshots = {}
-    var preloaded_video_quality = ''
-    var relatedVideos = []
-    var pornstar = []
-    var videodetails = {}
-    var noVideos = false
-    var serverError = false
-
-
-    try {
-
-        // const rawResponse = await fetch(`https://clownfish-app-jn7w9.ondigitalocean.app/getVideoPageDetails`, {
-        //     method: 'POST',
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify({ href: `https://spankbang.party/${keyy}/video/${title}` })
-        // });
-
-        // const resData = await rawResponse.json();
-        // const data = resData.data;
-     
-
-        const data = await getVideoPageData(`https://spankbang.party/${keyy}/video/${title}`)
-        videolink_qualities_screenshots = data.videolink_qualities_screenshots
-        preloaded_video_quality = data.preloaded_video_quality
-        relatedVideos = data.relatedVideos
-        pornstar = data.pornstar
-        videodetails = data.video_details
-        noVideos = data.noVideos
-
-    } catch (error) {
-        serverError = true
-    }
-
-
-
-    return {
-        props: {
-            videolink_qualities_screenshots: videolink_qualities_screenshots,
-            preloaded_video_quality: preloaded_video_quality,
-            relatedVideos: relatedVideos,
-            pornstar: pornstar,
-            video_details: videodetails,
-            videoTitleBackUp: title,
-            noVideo: noVideos,
-            serverError: serverError,
-        }
-    }
-
-
-}
 
 
 
