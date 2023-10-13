cf640f89fee6e84b21fa82842f6dc809c5d41d43
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2023-10-13 22:44:05 +0545

    commit

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,12 +1,19 @@
-export default async function handler(req, res) {
 
-    try {
-        await res.revalidate('/')
-        await res.revalidate('/photo')
-        console.log('Re-Validating... Successfull');
-        return res.json({ revalidated: true })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send("Error Re-Validating:")
-    }
+export default async function handler(req, res) {
+    
+    // Check for secret to confirm this is a valid request
+ if (req.query.secret !== "sadfsadfdsafdsafasdfsdafdsafsadfdsaf") {
+   return res.status(401).json({ message: 'Invalid token' })
+ }
+
+   try {
+    await res.revalidate('/')
+    await res.revalidate('/photo')
+    console.log('Re-Validating... Successfull');
+    return res.json({ revalidated: true })
+   } catch (error) {
+       console.log(error);
+       return res.status(500).send(error)
+   }
 }
+
