b9340e9f56a2a30b7a1f68a3b9ea2e02d53fe89d
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2023-10-26 23:39:15 +0545

    commit

--- components/Ads/Popunder_Mob.js
+++ components/Ads/Popunder_Mob.js
@@ -10,29 +10,7 @@ function PopunderAds() {
         <div className="flex items-center justify-center">
 
 
-            <Script
-                id="popunder"
-                strategy="beforeInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    var ad_idzone = "5069578",
-    ad_popup_fallback = true,
-    ad_popup_force = false,
-    ad_chrome_enabled = true,
-    ad_new_tab = true,
-    ad_frequency_period = 5,
-    ad_frequency_count = 3,
-    ad_trigger_method = 3,
-    ad_trigger_delay = 0; 
-  `,
-                }}
-            />
-
-
-            <Script
-                strategy="afterInteractive"
-                src="https://a.pemsrv.com/popunder1000.js"
-            />
+            <script src="/AdsScript/PopunderScript_Mob.js"></script>
 
 
 
--- components/Ads/Popunder_Web.js
+++ components/Ads/Popunder_Web.js
@@ -7,30 +7,7 @@ function PopunderAds_2() {
     return (
         <div className="flex items-center justify-center">
 
-            <Script
-                id="popunder"
-                strategy="beforeInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    var ad_idzone = "5069558",
-                    ad_popup_fallback = true,
-                    ad_popup_force = false,
-                    ad_chrome_enabled = true,
-                    ad_new_tab = true,
-                    ad_frequency_period = 5,
-                    ad_frequency_count = 3,
-                    ad_trigger_method = 3,
-                    ad_trigger_delay = 0; 
-  `,
-                }}
-            />
-
-
-            <Script
-                strategy="afterInteractive"
-                src="https://a.pemsrv.com/popunder1000.js"
-            />
-
+            <script src="/AdsScript/PopunderScript_Web.js"></script>
 
 
 
