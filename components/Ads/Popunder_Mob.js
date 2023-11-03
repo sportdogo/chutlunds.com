3cb4cc041f83d68cebb87ca4fca0fa9f0aa42870
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2023-11-04 00:31:55 +0545

    commit

--- components/Ads/Popunder_Mob.js
+++ components/Ads/Popunder_Mob.js
@@ -8,16 +8,17 @@ function PopunderAds() {
 
 
 
-            {/* <Script
+            <Script
                 type="text/javascript"
                 src="//cdn.tsyndicate.com/sdk/v1/p.js"
                 data-ts-spot="0fede73077d5463e9a84946bbe3c27ff"
                 data-ts-extid="{extid}"
-                data-ts-session-duration="5"
+                data-ts-session-duration="300"
                 data-ts-count="3"
+                
                 async
                 defer
-            /> */}
+            />
 
 
 
--- components/Ads/Popunder_Web.js
+++ components/Ads/Popunder_Web.js
@@ -7,16 +7,16 @@ function PopunderAds_2() {
     return (
         <div className="flex items-center justify-center">
 
-            {/* <Script
+            <Script
                 type="text/javascript"
                 src="//cdn.tsyndicate.com/sdk/v1/p.js"
                 data-ts-spot="637f41ccc95a46769276024b3fe07174"
                 data-ts-extid="{extid}"
-                data-ts-session-duration="5"
+                data-ts-session-duration="300"
                 data-ts-count="3"
                 async
                 defer
-            /> */}
+            />
 
         </div>
     )
