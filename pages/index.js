5c2e7b15a7716b83fea64bc75b9d954b29feed29
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2023-11-23 23:55:54 +0545

    commit

--- pages/index.js
+++ pages/index.js
@@ -195,7 +195,7 @@ export default function Home({ video_collection, pages, desiVideosDataArray, des
 export async function getStaticProps({ req, res }) {
 
   const parcelData = { href: "https://spankbang.party/" }
-  const rawResponse = await fetch(`https://chutlunds-com-puce.vercel.app/api/spangbang/homepage`, {
+  const rawResponse = await fetch(`https://chutlunds.com/api/spangbang/homepage`, {
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
