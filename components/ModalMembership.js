ae153405944e0c4164551c791348bdf74e0101dd
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-03-10 01:02:12 +0545

    sdafsda

--- components/ModalMembership.js
+++ components/ModalMembership.js
@@ -87,46 +87,46 @@ const ModalMembership = () => {
 
             <div className={`bg-white w-4/5 lg:w-2/4 p-[20px] rounded-xl shadow-md `}>
 
-                <div class="w-full pt-1 pb-5">
-                    <div class="bg-indigo-500 text-white overflow-hidden rounded-full w-20 h-20 -mt-16 mx-auto shadow-lg flex justify-center items-center">
-                        <i class="mdi mdi-credit-card-outline text-3xl"></i>
+                <div className="w-full pt-1 pb-5">
+                    <div className="bg-indigo-500 text-white overflow-hidden rounded-full w-20 h-20 -mt-16 mx-auto shadow-lg flex justify-center items-center">
+                        <i className="mdi mdi-credit-card-outline text-3xl"></i>
                     </div>
                 </div>
-                <div class="mb-10">
-                    <h1 class="text-center font-bold text-xl uppercase">Secure payment info</h1>
+                <div className="mb-10">
+                    <h1 className="text-center font-bold text-xl uppercase">Secure payment info</h1>
                 </div>
-                <div class="mb-3 flex -mx-2 items-center">
-                    <div class="px-2">
-                        <label for="type1" class="flex items-center cursor-pointer">
-                            <input type="radio" class="form-radio h-5 w-5 text-indigo-500" name="type" id="type1" checked />
-                            <img src="https://leadershipmemphis.org/wp-content/uploads/2020/08/780370.png" class="h-8 ml-3" />
+                <div className="mb-3 flex -mx-2 items-center">
+                    <div className="px-2">
+                        <label htmlFor="type1" className="flex items-center cursor-pointer">
+                            <input type="radio" className="form-radio h-5 w-5 text-indigo-500" name="type" id="type1" checked />
+                            <img src="https://leadershipmemphis.org/wp-content/uploads/2020/08/780370.png" className="h-8 ml-3" />
                         </label>
                     </div>
-                    <div class="px-2">
-                        <label for="type2" class="flex items-center cursor-pointer">
-                            <input type="radio" class="form-radio h-5 w-5 text-indigo-500" name="type" id="type2" />
-                            <img src="https://www.sketchappsources.com/resources/source-image/PayPalCard.png" class="h-12 ml-3" />
+                    <div className="px-2">
+                        <label htmlFor="type2" className="flex items-center cursor-pointer">
+                            <input type="radio" className="form-radio h-5 w-5 text-indigo-500" name="type" id="type2" />
+                            <img src="https://www.sketchappsources.com/resources/source-image/PayPalCard.png" className="h-12 ml-3" />
                         </label>
                     </div>
                 </div>
 
-                <div class="mb-3">
-                    <label class="font-bold text-sm mb-2 ml-1">Name on card</label>
+                <div className="mb-3">
+                    <label className="font-bold text-sm mb-2 ml-1">Name on card</label>
                     <div>
-                        <input class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="John Smith" type="text" />
+                        <input className="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="John Smith" type="text" />
                     </div>
                 </div>
-                <div class="mb-3">
-                    <label class="font-bold text-sm mb-2 ml-1">Card number</label>
+                <div className="mb-3">
+                    <label className="font-bold text-sm mb-2 ml-1">Card number</label>
                     <div>
-                        <input class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="0000 0000 0000 0000" type="text" />
+                        <input className="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="0000 0000 0000 0000" type="text" />
                     </div>
                 </div>
-                <div class="mb-3 -mx-2 flex items-end">
-                    <div class="px-2 w-1/2">
-                        <label class="font-bold text-sm mb-2 ml-1">Expiration date</label>
+                <div className="mb-3 -mx-2 flex items-end">
+                    <div className="px-2 w-1/2">
+                        <label className="font-bold text-sm mb-2 ml-1">Expiration date</label>
                         <div>
-                            <select class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                            <select className="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
                                 <option value="01">01 - January</option>
                                 <option value="02">02 - February</option>
                                 <option value="03">03 - March</option>
@@ -142,8 +142,8 @@ const ModalMembership = () => {
                             </select>
                         </div>
                     </div>
-                    <div class="px-2 w-1/2">
-                        <select class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                    <div className="px-2 w-1/2">
+                        <select className="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
                             <option value="2024">2024</option>
                             <option value="2025">2025</option>
                             <option value="2026">2026</option>
@@ -156,14 +156,14 @@ const ModalMembership = () => {
                         </select>
                     </div>
                 </div>
-                <div class="mb-10">
-                    <label class="font-bold text-sm mb-2 ml-1">Security code</label>
+                <div className="mb-10">
+                    <label className="font-bold text-sm mb-2 ml-1">Security code</label>
                     <div>
-                        <input class="w-32 px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="000" type="number" />
+                        <input className="w-32 px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="000" type="number" />
                     </div>
                 </div>
                 <div>
-                    <button class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"><i class="mdi mdi-lock-outline mr-1"></i> PAY NOW  ($2.99)</button>
+                    <button className="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"><i className="mdi mdi-lock-outline mr-1"></i> PAY NOW  ($2.99)</button>
                 </div>
             </div>
 
