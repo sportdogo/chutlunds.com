9ce973da0915e1d3429728531cc42d7c7de10c51
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-03-12 08:24:09 +0545

    ,lkn

--- pages/membership.js
+++ pages/membership.js
@@ -48,7 +48,7 @@ const Membership = () => {
                 </div>
 
 
-                <button onClick={() => setpaymentModalVisible(true)} className='bg-theme text-white lg:px-8 lg:py-4 px-6 py-3 rounded-2xl font-poppins text-[14px] lg:text-[20px] mx-auto block  hover:scale-105 transition-all mt-4 lg:mt-8'>Get Membership now!</button>
+                <button onClick={() => setpaymentModalVisible(true)} className='bg-theme text-white lg:px-8 lg:py-4 px-6 py-3 rounded-2xl font-poppins text-[14px] lg:text-[20px] mx-auto block  hover:scale-105 transition-all mt-4 lg:mt-8'>Get Access now!</button>
 
 
                 <div className='fixed bottom-0 lg:fixed p-6 gap-6 left-0 grid grid-cols-2 lg:grid-cols-4 bg-black bg-opacity-70  w-full'>
