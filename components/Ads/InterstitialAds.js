d2221c0863634f9911f23e66c5f4c94e844c0b65
bhola <bhola2266@gmail.com>
2024-07-10 03:03:41 +0545

    asdfdsa

--- components/Ads/InterstitialAds.js
+++ components/Ads/InterstitialAds.js
@@ -1,27 +1,27 @@
 // components/InterstitialAd.js
-import { useEffect } from 'react';
 import Head from 'next/head';
 import Script from 'next/script';
 
 const InterstitialAds = () => {
-    useEffect(() => {
-        if (typeof InterstitialTsAd !== 'undefined') {
-            InterstitialTsAd({
-                spot: "ee9ab24036174503b1debe6c3ccc7fdf",
-                extid: "{extid}",
-            });
-        }
-    }, []);
-
     return (
         <>
             <Head>
                 <link rel="stylesheet" href="//cdn.tsyndicate.com/sdk/v1/interstitial.ts.css" />
             </Head>
-            <Script src="//cdn.tsyndicate.com/sdk/v1/interstitial.ts.js" strategy="afterInteractive" />
+            <Script
+                src="//cdn.tsyndicate.com/sdk/v1/interstitial.ts.js"
+                strategy="afterInteractive"
+                onLoad={() => {
+                    InterstitialTsAd({
+                        spot: "ee9ab24036174503b1debe6c3ccc7fdf",
+                        extid: "{extid}",
+                    });
+                }}
+            />
             <div id="interstitial-ad"></div>
         </>
     );
 };
+
 
 export default InterstitialAds;
--- pages/rough.js
+++ pages/rough.js
@@ -1,16 +1,14 @@
-import React, { useEffect } from 'react'
-import Outstreams from '../components/Ads/Outstream'
-import { getLocation } from '../config/firebase/lib'
-import InterstitialAds from '@/components/Ads/InterstitialAds'
+import InterstitialAds from '@/components/Ads/InterstitialAds';
+import React from 'react';
+import Link from 'next/link';
 
 export default function Rough() {
-
-
     return (
         <div>
-
-            <a href='/'>Click me</a>
+            <Link href="/">
+                <a>Click me</a>
+            </Link>
             <InterstitialAds />
         </div>
-    )
+    );
 }
